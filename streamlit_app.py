84fa78dcc442850859ac502bc91e54789e05a650
liendl <john.l.t.serrano@accenture.com>
2024-10-07 12:25:44 +0800

    Update streamlit_app.py
    dataframe added

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
+#st.text(fruityvice_response.json())
+fv_df = st.dataFrame(data = fruityvice_response.json(), use_container_width = True)
 
 
 

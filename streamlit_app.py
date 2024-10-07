6e9b4fed0584b5d22de42fdaa0aeab95af8f0fde
liendl <john.l.t.serrano@accenture.com>
2024-10-07 12:28:31 +0800

    Update streamlit_app.py
    new display

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,10 +42,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
+#new section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-fv_df = st.dataFrame(data = fruityvice_response.json(), use_container_width = True)
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
 
 
